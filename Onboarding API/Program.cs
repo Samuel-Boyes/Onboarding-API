0ffc7ab1ceb8343c70b3e039fcb5d70a479c4aea
Samuel Boyes <sjboyes96@gmail.com>
2024-01-11 18:14:06 +1300

    Redeploy change

--- Onboarding API/Program.cs
+++ Onboarding API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader()
         .AllowAnyMethod());
 });
-
+//
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
